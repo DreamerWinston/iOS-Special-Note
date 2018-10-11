c8f53a02cff7790c9fe8bcc86fc0d89fbe60a5f9
Winston <wwfloveforever@gmail.com>
2018-10-11 12:48:04 +0800

    上传demo

---
+++ StudyDevelopDemo/StudyDevelopDemoTests/StudyDevelopDemoTests.m
@@ -0,0 +1,37 @@
+//
+//  StudyDevelopDemoTests.m
+//  StudyDevelopDemoTests
+//
+//  Created by winston on 2018/10/11.
+//  Copyright © 2018 winston. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface StudyDevelopDemoTests : XCTestCase
+
+@end
+
+@implementation StudyDevelopDemoTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
